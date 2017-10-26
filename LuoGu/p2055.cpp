d941eb9ce19c739776a90a426f169871634996a0
igronemyk <igronemyk@gmail.com>
2017-10-26 16:13:40 +0800

    Update

--- LuoGu/p2055.cpp
+++ LuoGu/p2055.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
@@ -10,6 +11,58 @@ T read() {
     return
 }
 
+const int MAXN = 60;
+
+namespace Adj {
+    struct Edge {
+        int next,to;
+    } edges[MAXN * MAXN];
+    int tot,heads[MAXN];
+
+    void init() {
+        tot = 0;
+        memset(heads,-1,sizeof(heads));
+    }
+
+    void addEdge(int u,int v) {
+        edges[tot].to = v;
+        edges[tot].next = heads[u];
+        heads[u] = tot++;
+    }
+}
+
+bool *match,*visit;
+
+
 int main() {
+    int T = read<int>();
+    while(T--) {
+        init();
+        int n = read<int>();
+        bool *isSchoolStudent = new bool[n],*isAtSchool = new bool[n];
+        for(int i = 0;i < n;i++) {
+            isSchoolStudent[i] = read<int>();
+        }
+        int needValue = 0;
+        for(int i = 0;i < n;i++) {
+            isAtSchool[i] = read<int>();
+            isAtSchool[i] = !isAtSchool[i];
+            if(isSchoolStudent[i] && isAtSchool[i]) {
+                addEdge(i,i);
+            }
+            if(!isSchoolStudent[i] || (isSchoolStudent[i] && isAtSchool[i])) {
+                needValue++;
+            }
+        }
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < n;j++) {
+                bool tmpValue = read<int>();
+                if(tmpValue && isSchoolStudent[j]) {
+                    addEdge(i,j);
+                }
+            }
+        }
+
+    }
     return 0;
 }
