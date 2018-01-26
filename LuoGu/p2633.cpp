1d87c49546bfb3975c3ec39c0982b96315a994ca
Igronemyk <igronemyk@gmail.com>
2018-01-26 16:35:37 +0800

    Update

--- LuoGu/p2633.cpp
+++ LuoGu/p2633.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+const int MAX_DEPTH = 18;
+const int INF = 0x3f3f3f3f;
+
 template<typename T>
 T read() {
     T result = 0;int f = 1;char c = getchar();
@@ -15,85 +18,208 @@ T read() {
     return result * f;
 }
 
+/*
+struct SegmentNode {
+    int ch[2],cnt;
+
+    SegmentNode() : cnt(0) {
+        ch[0] = 0;
+        ch[1] = 0;
+    }
+
+    SegmentNode(int lc,int rc,int cnt) : cnt(cnt) {
+        ch[0] = lc;
+        ch[1] = rc;
+    }
+
+} sn[MAX_SEGMENT_NODE_SIZE];
+
+int tmp[QUERY_TMP_SIZE];
+
+int segmentSize = 0;
+
+struct SegmentTree {
+    int root,left,right;
+
+    static int newNode() {
+        sn[++segmentSize] = SegmentNode();
+        return segmentSize;
+    }
+
+    static int newNode(int nodeSign) {
+        sn[++segmentSize] = SegmentNode(sn[nodeSign].ch[0],sn[nodeSign].ch[1],sn[nodeSign].cnt);
+        return segmentSize;
+    }
+
+    SegmentTree() : root(0) , left(0) , right(0) { }
+
+    SegmentTree(int left,int right) : left(left) , right(right) {
+        root = newNode();
+    }
+
+    SegmentTree(int root,int left,int right) : root(root) , left(left) , right(right) { }
+
+    void updateInfo(int now) {
+        sn[now].cnt = sn[sn[now].ch[0]].cnt + sn[sn[now].ch[1]].cnt;
+    }
+
+    void update(int pos,int val) {
+        update(root,left,right,pos,val);
+    }
+
+    void update(int now,int nowLeft,int nowRight,int pos,int val) {
+        if(nowLeft == nowRight) {
+            sn[now].cnt += val;
+            return;
+        }
+        int mid = (nowLeft + nowRight) >> 1;
+        if(pos <= mid) {
+            if(sn[now].ch[0] == 0) {
+                sn[now].ch[0] = newNode();
+            }else {
+                sn[now].ch[0] = newNode(sn[now].ch[0]);
+            }
+            update(sn[now].ch[0],nowLeft,mid,pos,val);
+        }else {
+            if(sn[now].ch[1] == 0) {
+                sn[now].ch[1] = newNode();
+            }else {
+                sn[now].ch[1] = newNode(sn[now].ch[1]);
+            }
+            update(sn[now].ch[1],mid + 1,nowRight,pos,val);
+        }
+        updateInfo(now);
+    }
+
+    static int queryKth(SegmentTree u,SegmentTree v,SegmentTree lca,SegmentTree lcaFa,int k) {
+        tmp[0] = u.root;
+        tmp[1] = v.root;
+        tmp[2] = lca.root;
+        tmp[3] = lcaFa.root;
+        return queryKth(u.left,u.right,k);
+    }
+
+    static int queryKth(int nowLeft,int nowRight,int k) {
+        if(nowLeft == nowRight) {
+            return nowLeft;
+        }
+        int cnt = sn[sn[tmp[0]].ch[0]].cnt + sn[sn[tmp[1]].ch[0]].cnt - sn[sn[tmp[2]].ch[0]].cnt - sn[sn[tmp[3]].ch[0]].cnt;
+        //printf("Ask range %d,%d,%d\n,first %d,second %d,third %d,forth %d,ans %d\n",left,(left + right) >> 1,right,sn[sn[tmp[0]].ch[0]].cnt,sn[sn[tmp[1]].ch[0]].cnt,sn[sn[tmp[2]].ch[0]].cnt,sn[sn[tmp[3]].ch[0]].cnt,cnt);
+        int mid = (nowLeft + nowRight) >> 1;
+        if(cnt < k) {
+            for(int i = 0;i < 4;i++) {
+                tmp[i] = sn[tmp[i]].ch[1];
+            }
+            return queryKth(mid + 1,nowRight,k - cnt);
+        }else {
+            for(int i = 0;i < 4;i++) {
+                tmp[i] = sn[tmp[i]].ch[0];
+            }
+            return queryKth(nowLeft,mid,k);
+        }
+    }
+
+    SegmentTree copy() {
+        return SegmentTree(newNode(root),left,right);
+    }
+
+} strees[MAXN];
+*/
+
 struct SegmentTree {
     struct Node {
         Node *leftChild,*rightChild;
-        int cnt,sum,left,right;
+        int left,right,cnt;
 
-        Node(int left,int right) : leftChild(NULL) , rightChild(NULL) , left(left) , right(right) { }
-        Node(Node *leftChild,Node *rightChild,int cnt,int sum,int left,int right) : leftChild(leftChild) , rightChild(rightChild) , cnt(cnt) , sum(sum) , left(left) , right(right) { }
+        Node(int left,int right) : left(left) , right(right) , cnt(0) {
+            leftChild = NULL;
+            rightChild = NULL;
+        }
+
+        Node(Node *lc,Node *rc,int left,int right,int cnt) : leftChild(lc) , rightChild(rc) , left(left) , right(right) ,cnt(cnt) { }
 
         Node *copy() {
-            return new Node(leftChild,rightChild,cnt,sum,left,right);
+            return new Node(leftChild,rightChild,left,right,cnt);
         }
     };
 
     Node *root;
 
-    SegmentTree(int n) {
-        root = new Node(0,n - 1);
-        build(root);
+    SegmentTree(int left,int right) {
+        root = new Node(left,right);
     }
 
-    SegmentTree(Node *root) {
-        root = root;
-    }
-
-    void updateInfo(Node *now) {
-        now->cnt = now->leftChild->cnt + now->rightChild->cnt;
-    }
-
-    void build(Node *now) {
-        if(now->left == now->right) {
-            now->cnt = 0;
-            return;
-        }
-        int mid = (now->left + now->right) >> 1;
-        now->leftChild = new Node(now->left,mid);
-        build(now->leftChild);
-        now->rightChild = new Node(mid + 1,now->right);
-        build(now->rightChild);
-        updateInfo(now);
-    }
-
-    void add(int pos) {
-        add(root,pos);
-    }
-
-    void add(Node *now,int pos) {
-        if(now->left == pos && now->right == pos) {
-            now->cnt++;
-            return;
-        }
-        int mid = (now->left + now->right) >> 1;
-        if(pos <= mid) {
-            now->leftChild = now->leftChild->copy();
-            add(now->leftChild,pos);
-        }else {
-            now->rightChild = now->rightChild->copy();
-            add(now->rightChild,pos);
-        }
-        updateInfo(now);
-    }
+    SegmentTree(Node *root) : root(root) { }
 
     SegmentTree *copy() {
         return new SegmentTree(root->copy());
     }
-};
 
-struct Discreter {
-    int *saveValues;
-    int saveSize;
-    Discreter(int *values,int size) {
-        saveValues = new int[size];
-        memcpy(saveValues,values,sizeof(int) * size);
-        sort(saveValues,saveValues + size);
-        saveSize = unique(saveValues,saveValues + size) - saveValues;
+    static int getCnt(Node *now) {
+        if(now == NULL) return 0;
+        return now->cnt;
     }
 
-    int getValue(int value) {
-        return lower_bound(saveValues,saveValues + saveSize,value) - saveValues;
+    static int getLeftChildCnt(Node *now) {
+        if(now == NULL) return 0;
+        return getCnt(now->leftChild);
     }
+
+    static Node *getLeftChild(Node *now) {
+        if(now == NULL) return NULL;
+        return now->leftChild;
+    }
+
+    static Node *getRightChild(Node *now) {
+        if(now == NULL) return NULL;
+        return now->rightChild;
+    }
+
+    static int queryKth(SegmentTree *u,SegmentTree *v,SegmentTree *lca,SegmentTree *lcaFa,int k) {
+        return queryKth(u->root->left,u->root->right,u->root,v->root,lca->root,lcaFa->root,k);
+    }
+
+    static int queryKth(int nowLeft,int nowRight,Node *nowU,Node *nowV,Node *nowLCA,Node *nowLCAFa,int k) {
+        if(nowLeft == nowRight) {
+            return nowLeft;
+        }
+        int mid = (nowLeft + nowRight) >> 1;
+        int cnt = getLeftChildCnt(nowU) + getLeftChildCnt(nowV) - getLeftChildCnt(nowLCA) - getLeftChildCnt(nowLCAFa);
+        if(cnt < k) {
+            return queryKth(mid + 1,nowRight,getRightChild(nowU),getRightChild(nowV),getRightChild(nowLCA),getRightChild(nowLCAFa),k - cnt);
+        }else {
+            return queryKth(nowLeft,mid,getLeftChild(nowU),getLeftChild(nowV),getLeftChild(nowLCA) , getLeftChild(nowLCAFa),k);
+        }
+    }
+
+    void update(int pos) {
+        update(root,pos);
+    }
+
+    void update(Node *now,int pos) {
+        now->cnt++;
+        if(now->left == now->right) {
+            return;
+        }
+        int mid = (now->left + now->right) >> 1;
+        if(pos <= mid) {
+            if(now->leftChild == NULL) {
+                now->leftChild = new Node(now->left,mid);
+            }else {
+                now->leftChild = now->leftChild->copy();
+            }
+            update(now->leftChild,pos);
+        }else {
+            if(now->rightChild == NULL) {
+                now->rightChild = new Node(mid + 1,now->right);
+            }else {
+                now->rightChild = now->rightChild->copy();
+            }
+            update(now->rightChild,pos);
+        }
+    }
+
+
 };
 
 struct Graph {
@@ -121,23 +247,85 @@ struct Graph {
 /*
 构建father
 构建主席树
-构建欧拉序
 */
-void dfs(int now,int father,Graph &graph,SegmentTree *trees) {
+void dfs(int now,int father,int *values,int *depths,int **fathers,SegmentTree **strees,Graph &graph) {
+    depths[now] = depths[father] + 1;
+    strees[now] = strees[father]->copy();
+    strees[now]->update(values[now]);
+    fathers[now][0] = father;
+    for(int i = graph.heads[now];i != - 1;i = graph.edges[i].next) {
+        Graph::Edge &tmpEdge = graph.edges[i];
+        if(tmpEdge.to == father) continue;
+        dfs(tmpEdge.to,now,values,depths,fathers,strees,graph);
+    }
+}
 
+void init(int N,int **fathers) {
+    for(int i = 1;i <= MAX_DEPTH;i++) {
+        for(int j = 1;j <= N;j++) {
+            fathers[j][i] = fathers[fathers[j][i - 1]][i - 1];
+        }
+    }
+}
+
+int LCA(int u,int v,int **fathers,int *depths) {
+    if(depths[u] < depths[v]) {
+        swap(u,v);
+    }
+    for(int i = MAX_DEPTH;i >= 0;i--) {
+        if(depths[fathers[u][i]] >= depths[v]) {
+            u = fathers[u][i];
+        }
+    }
+    if(u == v) {
+        return u;
+    }
+    for(int i = MAX_DEPTH;i >= 0;i--) {
+        if(fathers[u][i] != fathers[v][i]) {
+            u = fathers[u][i];
+            v = fathers[v][i];
+        }
+    }
+    return fathers[u][0];
 }
 
 int main() {
-    int N = read<int>(),M = read<int>(),*values = new int[N];
-    for(int i = 0;i < N;i++) {
-        values[i] = read<int>();
+    int N = read<int>(),M = read<int>(),*values = new int[N + 1],*sortedValues = new int[N],*sign = new int[N + 1],*depths = new int[N + 1],**fathers = new int*[N + 1];
+    depths[0] = 0;
+    values[0] = 0;
+    Graph graph((N - 1) * 2,N + 1);
+    for(int i = 0;i <= N;i++) {
+        fathers[i] = new int[MAX_DEPTH + 1];
+        memset(fathers[i],0,sizeof(int) * (MAX_DEPTH + 1));
     }
-    Discreter discreter(values,N);
-    Graph graph();
+    for(int i = 1;i <= N;i++) {
+        values[i] = read<int>();
+        sortedValues[i - 1] = values[i];
+    }
+    sort(sortedValues,sortedValues + N);
+    int sortSize = unique(sortedValues,sortedValues + N) - sortedValues;
+    for(int i = 1;i <= N;i++) {
+        int newSign = lower_bound(sortedValues,sortedValues + sortSize,values[i]) - sortedValues;
+        sign[newSign] = values[i];
+        values[i] = newSign;
+    }
     for(int i = 0;i < N - 1;i++) {
         int u = read<int>(),v = read<int>();
-        u--; v--;
         graph.addEdge(u,v);
+        graph.addEdge(v,u);
+    }
+    SegmentTree **strees = new SegmentTree*[N + 1];
+    strees[0] = new SegmentTree(0,N - 1);
+    dfs(1,0,values,depths,fathers,strees,graph);
+    init(N,fathers);
+    int lastAns = 0;
+    while(M--) {
+        int u = read<int>(),v = read<int>(),k = read<int>();
+        u ^= lastAns;
+        int lca = LCA(u,v,fathers,depths);
+        lastAns = sign[SegmentTree::queryKth(strees[u],strees[v],strees[lca],strees[fathers[lca][0]],k)];
+        printf("%d",lastAns);
+        if(M != 0) printf("\n");
     }
     return 0;
 }
---
+++ LuoGu/p3380-2.cpp
@@ -0,0 +1,315 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cstddef>
+#include <cmath>
+#include <utility>
+
+using namespace std;
+
+const int MAX_TREAP_NODE_SIZE = 2600000;
+const int MAX_SEGMENT_NODE_SIZE = 2600000;
+const int MIN_VAL_RANGE = 0;
+const int MAX_VAL_RANGE = 1e8;
+const int MAXN = 50010;
+const int INF = 2147483647;
+
+template<typename T>
+T read() {
+    T result = 0;int f = 1;char c = getchar();
+    while(c > '9' || c < '0') {if(c == '-') f *= -1;c = getchar();}
+    while(c <= '9' && c >= '0') {result = result * 10 + c - '0';c = getchar();}
+    return result * f;
+}
+
+struct TreapNode {
+    int ch[2],value,size;
+
+    TreapNode() : value(0) , size(0) {
+        ch[0] = 0;
+        ch[1] = 0;
+    }
+
+    TreapNode(int value) : value(value) , size(1) {
+        ch[0] = 0;
+        ch[1] = 0;
+    }
+
+} tn[MAX_TREAP_NODE_SIZE];
+
+int treapSize = 0;
+
+struct Treap {
+    int root;
+
+    typedef pair<int,int> DRoot;
+
+    Treap() : root(0) { }
+
+    static int newNode(int value) {
+        tn[++treapSize] = TreapNode(value);
+        return treapSize;
+    }
+
+    static void updateInfo(int now) {
+        tn[now].size = tn[tn[now].ch[0]].size + tn[tn[now].ch[1]].size + 1;
+    }
+
+    static int merge(int a,int b) {
+        if(a == 0) return b;
+        if(b == 0) return a;
+        bool flag = rand() % 2;
+        if(flag) {
+            tn[a].ch[1] = merge(tn[a].ch[1],b);
+            updateInfo(a);
+            return a;
+        }else {
+            tn[b].ch[0] = merge(a,tn[b].ch[0]);
+            updateInfo(b);
+            return b;
+        }
+    }
+
+    static DRoot split(int now,int k) {
+        DRoot result(0,0);
+        if(now == 0) return result;
+        if(k <= tn[tn[now].ch[0]].size) {
+            result = split(tn[now].ch[0],k);
+            tn[now].ch[0] = result.second;
+            updateInfo(now);
+            result.second = now;
+        }else {
+            result = split(tn[now].ch[1],k - tn[tn[now].ch[0]].size - 1);
+            tn[now].ch[1] = result.first;
+            updateInfo(now);
+            result.first = now;
+        }
+        return result;
+    }
+
+    int lowerCount(int value) {
+        int now = root,result = 0;
+        while(now != 0) {
+            if(tn[now].value < value) {
+                result += tn[tn[now].ch[0]].size + 1;
+                now = tn[now].ch[1];
+            }else {
+                now = tn[now].ch[0];
+            }
+        }
+        return result;
+    }
+
+    void insert(int value) {
+        int cnt = lowerCount(value);
+        DRoot tmp = split(root,cnt);
+        int newNodeSign = newNode(value);
+        root = merge(tmp.first,merge(newNodeSign,tmp.second));
+    }
+
+    void remove(int value) {
+        int cnt = lowerCount(value);
+        DRoot tmp = split(root,cnt);
+        root = merge(tmp.first,split(tmp.second,1).second);
+    }
+
+    int getCnt(int left,int right) {
+        int leftCount = lowerCount(left),rightCount = lowerCount(right + 1);
+        return rightCount - leftCount;
+    }
+
+    void print() {
+        print(root);
+        printf("\n");
+    }
+
+    void print(int now) {
+        if(now == 0) return;
+        print(tn[now].ch[0]);
+        printf("%d ",tn[now].value);
+        print(tn[now].ch[1]);
+    }
+};
+
+struct SegmentNode {
+    int ch[2],left,right;
+    Treap treap;
+
+    SegmentNode() : left(left) , right(right) {
+        ch[0] = 0;
+        ch[1] = 0;
+        treap = Treap();
+    }
+
+    SegmentNode(int left,int right) : left(left) , right(right) {
+        ch[0] = 0;
+        ch[1] = 0;
+        treap = Treap();
+    }
+
+    bool isLeafNode() {
+        return left == right;
+    }
+} sn[MAX_SEGMENT_NODE_SIZE];
+
+int segmentSize = 0;
+
+struct SegmentTree {
+    int root;
+
+    static int newNode(int left,int right) {
+        sn[++segmentSize] = SegmentNode(left,right);
+        return segmentSize;
+    }
+
+    SegmentTree(int left,int right) {
+        root = newNode(left,right);
+    }
+
+    void insert(int pos,int val) {
+        insert(root,pos,val);
+    }
+
+    void insert(int now,int pos,int val) {
+        sn[now].treap.insert(val);
+        if(sn[now].isLeafNode()) return;
+        int mid = (sn[now].left + sn[now].right) >> 1;
+        if(pos <= mid) {
+            if(sn[now].ch[0] == 0) {
+                sn[now].ch[0] = newNode(sn[now].left,mid);
+            }
+            insert(sn[now].ch[0],pos,val);
+        }else {
+            if(sn[now].ch[1] == 0) {
+                sn[now].ch[1] = newNode(mid + 1,sn[now].right);
+            }
+            insert(sn[now].ch[1],pos,val);
+        }
+    }
+
+    void remove(int pos,int val) {
+        remove(root,pos,val);
+    }
+
+    void remove(int now,int pos,int val) {
+        sn[now].treap.remove(val);
+        if(sn[now].isLeafNode()) return;
+        int mid = (sn[now].left + sn[now].right) >> 1;
+        if(pos <= mid) {
+            remove(sn[now].ch[0],pos,val);
+        }else {
+            remove(sn[now].ch[1],pos,val);
+        }
+    }
+
+    int queryLessThan(int left,int right,int value) {
+        return queryCnt(root,0,value - 1,left,right);
+    }
+
+    int queryNotGreaterThan(int left,int right,int value) {
+        return queryCnt(root,0,value,left,right);
+    }
+
+    int queryCnt(int now,int left,int right,int queryLeft,int queryRight) {
+        if(now == 0) return 0;
+        if(sn[now].left == left && sn[now].right == right) {
+            int tmpResult = sn[now].treap.getCnt(queryLeft,queryRight);
+            printf("At %d,%d query %d,%d,result %d\n",left,right,queryLeft,queryRight,tmpResult);
+            sn[now].treap.print();
+            return tmpResult;
+        }
+        int mid = (sn[now].left + sn[now].right) >> 1;
+        if(left <= mid) {
+            if(right <= mid) {
+                return queryCnt(sn[now].ch[0],left,right,queryLeft,queryRight);
+            }else {
+                return queryCnt(sn[now].ch[0],left,mid,queryLeft,queryRight) + queryCnt(sn[now].ch[1],mid + 1,right,queryLeft,queryRight);
+            }
+        }else {
+            return queryCnt(sn[now].ch[1],left,right,queryLeft,queryRight);
+        }
+    }
+
+    int queryValue(int left,int right,int rank) {
+        return queryValue(root,left,right,rank);
+    }
+
+    int queryValue(int now,int left,int right,int rank) {
+        if(sn[now].isLeafNode()) {
+            return sn[now].left;
+        }
+        int cnt = sn[sn[now].ch[0]].treap.getCnt(left,right);
+        if(cnt < rank) {
+            return queryValue(sn[now].ch[1],left,right,rank - cnt);
+        }else {
+            return queryValue(sn[now].ch[0],left,right,rank);
+        }
+    }
+
+    int queryPrecursor(int left,int right,int value) {
+        int cnt = queryLessThan(left,right,value);
+        if(cnt == 0) return -INF;
+        return queryValue(left,right,cnt);
+    }
+
+    int querySuccessor(int left,int right,int value) {
+        int cnt = queryNotGreaterThan(left,right,value);
+        int tmpValue = queryValue(left,right,cnt + 1);
+        if(tmpValue == value) {
+            if(cnt + 1 == right - left + 1) return INF;
+            return queryValue(left,right,cnt + 2);
+        }else {
+            return queryValue(left,right,cnt + 1);
+        }
+    }
+};
+
+int values[MAXN];
+
+int main() {
+    int n = read<int>(),m = read<int>();
+    SegmentTree stree(MIN_VAL_RANGE,MAX_VAL_RANGE);
+    for(int i = 0;i < n;i++) {
+        values[i] = read<int>();
+        stree.insert(values[i],i);
+    }
+    while(m--) {
+        int opt = read<int>();
+        switch(opt) {
+            case 1: {
+                int l = read<int>(),r = read<int>(),k = read<int>();
+                l--; r--;
+                printf("%d\n",stree.queryLessThan(l,r,k) + 1);
+                break;
+            }
+            case 2: {
+                int l = read<int>(),r = read<int>(),k = read<int>();
+                l--; r--;
+                printf("%d\n",stree.queryValue(l,r,k));
+                break;
+            }
+            case 3: {
+                int pos = read<int>(),k = read<int>();
+                pos--;
+                stree.remove(values[pos],pos);
+                values[pos] = k;
+                stree.insert(values[pos],pos);
+                break;
+            }
+            case 4: {
+                int l = read<int>(),r = read<int>(),k = read<int>();
+                l--; r--;
+                printf("%d\n",stree.queryPrecursor(l,r,k));
+                break;
+            }
+            case 5: {
+                int l = read<int>(),r = read<int>(),k = read<int>();
+                l--; r--;
+                printf("%d\n",stree.querySuccessor(l,r,k));
+                break;
+            }
+        }
+    }
+    return 0;
+}
+//TODO Fix the query value could not read the real left or right value from the node infomation,so change it to the arguments.
